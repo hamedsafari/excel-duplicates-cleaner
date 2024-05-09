2ca386fc9097323688e6310ae143a3eebc70143b
Hamed Safari <hamed.safari1990@gmail.com>
2024-05-09 12:21:15 +0330

    disabled debug mode and fixed static file serving.

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-em*7rz5t)1_*zdfc0!l#z1fjj_3^fcgklqvbf&%@7d54i4%=n6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
