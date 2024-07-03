2fb1c4cab19f696fbbb9887db9843f93dc75f6f1
Hamed Safari <hamed.safari1990@gmail.com>
2024-07-03 10:27:12 +0330

    added test view for google calendar event watch.

--- apps/rest/urls.py
+++ apps/rest/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from apps.rest.views import ExcelDuplicateCleanerAPIView
+from apps.rest.views import ExcelDuplicateCleanerAPIView, GoogleCalendarAPIView
 
 urlpatterns = [
     path(
         "duplicates-cleaner/",
         ExcelDuplicateCleanerAPIView.as_view(),
         name="duplicates-cleaner",
-    )
+    ),
+    path("calendar/", GoogleCalendarAPIView.as_view(), name="calendar"),
 ]
